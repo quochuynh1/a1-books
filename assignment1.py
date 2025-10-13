fda79493064b9a8dac5ea101fe9265fb7f908995
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-10-13 17:00:06 +1000

    Make a start with assignment1.py, initiating main menu, and create first function to print books

--- assignment1.py
+++ assignment1.py
@@ -7,7 +7,25 @@ GitHub URL:
 
 
 def main():
-    """..."""
+    "Program to keep track of books to read"""
+    print("Books to Read 1.0 by Quoc Huynh")
+    pass
+    print("Menu: ")
+    print("D - Display books")
+    print("A - Add a new book")
+    print("C - Complete a book")
+    print("Q - Quit")
+    user_input = input(">>> ").upper()
+    while user_input != "Q":
+        if user_input == "D":
+            print_books()
+        user_input = input(">>> ")
+
+def print_books():
+    in_file = open("books.csv", "r")
+    for line in in_file:
+        print(line)
+    in_file.close()
 
 
 if __name__ == '__main__':
