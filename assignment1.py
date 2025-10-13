324810e87cefe2b1f84cdbe896f28319feb5dc20
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-10-13 17:16:38 +1000

    Fix so that in_file is only loaded once at the start of the program

--- assignment1.py
+++ assignment1.py
@@ -10,6 +10,9 @@ def main():
     "Program to keep track of books to read"""
     print("Books to Read 1.0 by Quoc Huynh")
     pass
+
+    in_file = open("books.csv", "r")
+
     print("Menu: ")
     print("D - Display books")
     print("A - Add a new book")
@@ -18,14 +21,23 @@ def main():
     user_input = input(">>> ").upper()
     while user_input != "Q":
         if user_input == "D":
-            print_books()
-        user_input = input(">>> ")
+            print_books(in_file)
+        elif user_input == "A":
+            pass
+        elif user_input == "C":
+            pass
+        else:
+            print("Invalid menu choice")
+        user_input = input(">>> ").upper()
+    print("Quitting Program...")
 
-def print_books():
-    in_file = open("books.csv", "r")
-    for line in in_file:
-        print(line)
     in_file.close()
+
+def print_books(in_file):
+    for line in in_file:
+        parts = line.strip()
+        print(parts)
+
 
 
 if __name__ == '__main__':
